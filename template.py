578e00cb96c6a76e53e020534c5679ab44bf3d4a
dytsou <d.y.tsou@gmail.com>
2023-08-06 19:57:45 +0800

    send var

--- template.py
+++ template.py
@@ -1,11 +1,17 @@
 # $pip install flask
 from flask import Flask   #import Flask module
 from flask import render_template
+from datetime import datetime
 app = Flask(__name__)     #set up Flask object
 
 @app.route('/', methods=['GET']) #set web route '/'
 def hello(): #set func.
     return render_template('hello.html')
+
+@app.route('/hello/<string:name>')
+def time(name):
+    now = datetime.now() #present time
+    return render_template('hello2.html', **locals())
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
