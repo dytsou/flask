d8061745743140fa0165d87b0e5878157675e489
dytsou <d.y.tsou@gmail.com>
2023-08-07 14:43:22 +0800

    sql

---
+++ db.py
@@ -0,0 +1,27 @@
+from flask import Flask,  request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dytsou:dyt50u@127.0.0.1:5432/testdb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class student(db.Model):
+      __tablename__ = 'students'
+      sid = db.Column(db.Integer, primary_key=True)
+      name = db.Column(db.String(50), nullable=False)
+      tel = db.Column(db.String(50))
+      addr = db.Column(db.String(200))
+      email = db.Column(db.String(100))
+
+      def __init__(self, name, tel, addr, email):
+            self.name = name
+            self.tel = tel
+            self.addr = addr
+            self.email = email
+@app.route('/')
+def index():
+      db.create_all()
+      return "Success"
+if __name__ == '__main__':
+      app.run()
